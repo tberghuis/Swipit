8215b53751a91470b5753df30cfe6b0d43d73926
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-01-05 09:25:54 +1100

    .

--- app/src/main/java/dev/tberghuis/swipit/tmp/RedgifsActivity.kt
+++ app/src/main/java/dev/tberghuis/swipit/tmp/RedgifsActivity.kt
@@ -1,31 +1,16 @@
 package dev.tberghuis.swipit.tmp
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.media3.common.MediaItem
-import androidx.media3.ui.PlayerView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.tberghuis.swipit.ui.theme.SwipitTheme
 import dev.tberghuis.swipit.util.logd
-import kotlinx.coroutines.Deferred
 
 @AndroidEntryPoint
 class RedgifsActivity : ComponentActivity() {
@@ -40,4 +25,29 @@ class RedgifsActivity : ComponentActivity() {
       }
     }
   }
+
+
+  override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+
+    logd("keyCode: $keyCode")
+
+    return true
+
+//    return when (keyCode) {
+//      KeyEvent.KEYCODE_D -> {
+//        true
+//      }
+//      KeyEvent.KEYCODE_F -> {
+//        true
+//      }
+//      KeyEvent.KEYCODE_J -> {
+//        true
+//      }
+//      KeyEvent.KEYCODE_K -> {
+//        true
+//      }
+//      else -> super.onKeyUp(keyCode, event)
+//    }
+  }
+
 }
